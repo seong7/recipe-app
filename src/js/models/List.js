ad20d5f39b231f11677218858da0941471f56d4e
Seongjin Kim <seongjinkim1123@gmail.com>
2020-02-21 18:23:10 +0900

    패키지 uniqId down 후 적용

--- src/js/models/List.js
+++ src/js/models/List.js
@@ -1,3 +1,5 @@
+import uniqid from 'uniqid';
+
 export default class List {
   constructor() {
     this.items = [];
@@ -5,9 +7,21 @@ export default class List {
 
   addItem(count, unit, ingredient) {
     const item = {
+      id: uniqid(),
       count,
       unit,
       ingredient,
     };
+    this.items.push(item);
+  }
+
+  deleteItem(id) {
+    const index = this.items.findIndex((el) => el.id === id); // callback fn 이 true return 되는 첫 idx
+    /*
+      splice 와 slice 비교 :
+      [2, 4, 8].splice(1, 2) -> returns [4, 8],  original array is [2]  // original array 변함
+               .slice(1, 2) -> returns 4,  original array is [2, 4, 8]  // original array 변화 없음
+    */
+    this.items.splice(index, 1); // findIndex 로 찾은 idx 의 요소 하나만 삭제
   }
 }
